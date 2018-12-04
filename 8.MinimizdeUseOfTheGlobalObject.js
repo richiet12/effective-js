2e90089c2b39b2d9d8e9911bc094022ab0c99577
Richard Thurgood <rich.thurgood@gmail.com>
2018-12-04 07:41:51 +0000

    - add 8

---
+++ 8.MinimizdeUseOfTheGlobalObject.js
@@ -0,0 +1,6 @@
+// Things to remember
+
+// Avoid declaring global variables
+// Declare variables as locally as possible
+// Avoid adding properties to the global object
+// Use the global object for platform feature detection
